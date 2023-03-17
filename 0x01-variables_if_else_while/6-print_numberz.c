50db7bfddb0106b23d37400d743352b063bb1c25
ahmedhalimx <ahmedhalimcs101@example.com>
2023-03-17 13:18:31 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the silly version of '5-print_numbers.c',
+ * 	cause we need to do hard things even if they are useless ¯\_(ツ)_/¯.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
