b993e6bdf21f02058a18ee6bb5f362309a25e685
ahmedhalimx <ahmedhalimcs101@example.com>
2023-03-21 12:39:15 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all english alphabets in lowercase.
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	for (int i = 0; i <= 9; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		
+		_putchar('\n');
+	}
+}
