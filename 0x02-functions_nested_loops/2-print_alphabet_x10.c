8080c5ae47499792dcf5a9133b3d41f820e29207
ahmedhalimx <ahmedhalimcs101@example.com>
2023-03-21 12:49:58 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,9 @@ void print_alphabet_x10(void)
 {
 	char letter;
 	int i;
-	i=0;
-	for (i = 0; i <= 9; i++){
+	i = 0;
+	for (i = 0; i <= 9; i++)
+	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
