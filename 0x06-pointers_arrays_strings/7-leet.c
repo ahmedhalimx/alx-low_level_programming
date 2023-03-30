64e7a0a74b6135e53bdb3e336971d9de3e73c5c1
ahmedhalimx <ahmedhalimcs101@example.com>
2023-03-30 08:08:09 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - translate to leetspeak
+ * @n: string argumnent
+ * Return: @n
+ */
+char *leet(char *n)
+{
+	int x, y;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (x = 0; n[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (n[x] == s1[y])
+			{
+				n[x] = s2[y];
+			}
+		}
+	}
+	return (n);
+}
+
