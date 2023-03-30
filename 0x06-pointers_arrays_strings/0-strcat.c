d4a629ab5b84ee73dd5b78145ee7c24715fea68a
ahmedhalimx <ahmedhalimcs101@example.com>
2023-03-29 18:23:00 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <main.h>
+
+/**
+ *
+ * _strcat : appends the scr string to dest string.
+ *
+ * @scr : string to append with.
+ * @dest : string to append to.
+ *
+ * return : a pointer to the new string.
+ */
+char *_strcat(char *dest, char *src)()
+{
+	char *result;
+	result =  strcat(dest,src);
+	return result;
+}
