70585bcd4911591151998f9764e7d36214977f3d
ahmedhalimx <ahmedhalimcs101@example.com>
2023-04-12 08:16:28 -0700

    libmy.a

---
+++ 0x09-static_libiraries/_islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower- _islower function
+ *
+ * Return: (0) on Success
+ */
+
+int _islower(int c)
+{
+}
---
+++ 0x09-static_libiraries/_putchar.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * putchar - putchar function
+ *
+ * Return: (0) On Success
+ */
+int _putchar(char c)
+{
+}
