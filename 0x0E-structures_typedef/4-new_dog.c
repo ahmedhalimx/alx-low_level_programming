c6a74fe98bf446f77ef7546bd41373d2e8aee6d6
ahmedhalimx <ahmedhalimcs101@example.com>
2023-04-18 14:14:50 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,34 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptrdog;
+
+	ptrdog = malloc(sizeof(dog_t));
+
+	if (ptrdog == NULL)
+		return NULL;
+
+	ptrdog->name = malloc(strlen(name) + 1);
+	if (ptrdog->name == NULL)
+	{
+		free(ptrdog);
+		return (NULL);
+	}
+
+	ptrdog->owner = malloc(strlen(owner) + 1);
+	if (ptrdog->owner == NULL)
+	{
+		free(ptrdog->name);
+		free(ptrdog);
+		return (NULL);
+	}
+	
+	ptrdog->age = age;
+	strcpy(ptrdog->name, name);
+	strcpy(ptrdog->owner, owner);
+	return (ptrdog);
+}
