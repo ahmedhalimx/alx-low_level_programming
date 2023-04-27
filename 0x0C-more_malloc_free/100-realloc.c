f648504a510ae98e0804806ac4520fba738f8dae
Ahmed Halim <ahmedhalimcs101@gmail.com>
2023-04-27 09:06:18 -0700

    alx-low-level-programming

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,8 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *newptr;
+	ptr = (char *)ptr;
+	char *newptr;
 	unsigned int i;
 
 	if (new_size == 0 && ptr != NULL)
