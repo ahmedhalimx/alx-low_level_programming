defdb9174f7a9c8c4962712e9a62402353e2bef0
Ahmed Halim <ahmedhalimcs101@gmail.com>
2023-04-27 12:52:36 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,31 +1,38 @@
 #include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node_end - a function that adds a new node at the end of the list
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
  *
- * @head: pointer to first element in the list
- * @str: string to duplicate with
- *
- * Return: A pointer to the last node on success, or a NULL pointer otherwise
+ * Return: address of the new element, or NULL if it failed
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	list_t *tmp = *head;
+
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
-
-	new_node->str = malloc(strlen(str) + 1);
-
-	new_node->len = strlen(str);
+	
 	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 
-	while ((*head)->next != NULL)
-		(*head) = (*head)->next
+	if ((*head) == NULL)
+	{
+		(*head) = new_node;
+		return(new_node);
+	}
 
-	(*head)->next = new_node;
+	while (tmp->next)
+		tmp = tmp->next;
+
+	tmp->next = new_node;
 	return (new_node);
 }
 
