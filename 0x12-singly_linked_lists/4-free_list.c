ceed4ab3ea54338f3e2194c88494b23dd67c283a
Ahmed Halim <ahmedhalimcs101@gmail.com>
2023-04-27 13:05:49 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees an allocated memory on the heap for a list
+ *
+ * @head: a pointer that points to the first node in the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		free(head);
+		head = tmp->next;
+	}
+}
+
