9a686d267c2e7f2c39e3d1c603311bcf49144b3a
Ahmed Halim <ahmedhalimcs101@gmail.com>
2023-05-08 16:14:22 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	w = write(fd, text_content, strlen(text_content) - 1);
+	w = write(fd, text_content, strlen(text_content));
 
 	if (fd == -1 || w == -1)
 		return (-1);
